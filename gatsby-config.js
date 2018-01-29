ff3b229fdb49d4836c0b4cc7bbfb45b49d930118
Daniel Primo <delineas@gmail.com>
2018-01-29 15:45:39 +0100

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Default Podcast Starter',
   },
   plugins: [
-'gatsby-plugin-react-helmet',
-                {
-                        resolve: 'gatsby-source-feed',
-                        options: {
-                                feedURL: 'https://www.ivoox.com/podcast-1bit-memoria_fg_f1469800_filtro_1.xml'
-                        }
-                },
-],
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-feed',
+      options: {
+        feedURL: 'https://www.danielprimo.io/podcast/feed.xml'
+      }
+    },
+  ],
 };
